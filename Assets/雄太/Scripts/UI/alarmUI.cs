38594929f34980a60c53b8fe8ab8cd3a12422abf
meganenomegane1224 <104131253+meganenomegane1224@users.noreply.github.com>
2023-05-06 00:44:53 +0900

    コメント入れ終わりませんね

--- Assets/雄太/Scripts/UI/alarmUI.cs
+++ Assets/雄太/Scripts/UI/alarmUI.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class alarmUI : MonoBehaviour
 {
     [SerializeField]
-    private RectTransform rectTransform;
+    private RectTransform rectTransform;//自身のUIトランスフォームを取得
 private void Start() {
-    rectTransform.localPosition = new Vector3(0,0,0);
-    Destroy(rectTransform.gameObject,3);
+    rectTransform.localPosition = new Vector3(0,0,0);//ローカルポジションを中央にする
+    Destroy(rectTransform.gameObject,3);//３秒後に削除する
 }}
