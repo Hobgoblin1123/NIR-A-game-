f86b64f06b7aae757cb13d35a0140708e16fa044
meganenomegane1224 <104131253+meganenomegane1224@users.noreply.github.com>
2023-05-14 00:34:17 +0900

    不具合報告ボタンを作成

--- Assets/雄太/Scripts/Admin_Event.cs
+++ Assets/雄太/Scripts/Admin_Event.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 using UnityEngine;
 
 public class Admin_Event : MonoBehaviour
 {
     public int EventNumber = 0;
+    [SerializeField]
+    private PlayableDirector director;
+    [SerializeField]
+    private TimelineAsset[] timeline;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        director.Play(timeline[0]);
     }
 
     // Update is called once per frame
@@ -21,5 +27,14 @@ public class Admin_Event : MonoBehaviour
     public void NextEvent(int n)
     {
         EventNumber = n;
+        if(n == 1)
+        {
+            director.Play(timeline[1]);
+        }
+    }
+
+    public void OpneWebPage()
+    {
+        Application.OpenURL("https://nir-a.sakura.ne.jp/contact.html");
     }
 }
